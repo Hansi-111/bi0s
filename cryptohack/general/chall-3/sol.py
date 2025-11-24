2aa0d5805376a6c9fddcf7d7cb043b4138ea225a
Hansika L Chawla <hansikachawla111@gmail.com>
2025-11-24 20:45:44 +0530

    Update sol.py

--- cryptohack/general/chall-3/sol.py
+++ cryptohack/general/chall-3/sol.py
@@ -1,1 +1,18 @@
+##gcd
+def gcde(a, b):
+    global x, y    
+    if (a == 0):
+        x = 0
+        y = 1
+        return b
+    gcd = gcde(b % a, a)
+    x1 = x
+    y1 = y
+    x = y1 - (b // a) * x1
+    y = x1
+    return gcd
+a=66528
+b=52920 
+print(gcde(a,b))
 
+#
